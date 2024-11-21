3d979e2ec49926ac7e27c03d4c74387d1950c7f9
alonitzko <103517370+alonitzko@users.noreply.github.com>
2024-11-21 14:58:37 +0200

    Update IR_map_correction.R

--- Codes/maps_preparation/IR_map_correction.R
+++ Codes/maps_preparation/IR_map_correction.R
@@ -4,17 +4,18 @@ pwd = dirname(dirname(dirname(rstudioapi::getActiveDocumentContext()$path)))
 
 flight = 'Zeelim_31.05.21_1516/'
 map_name = 'IX-12-11646_0206_index_thermal_ir_cropped_5.tif'
-# Step 1: Load the thermal image file
+
+#Load the thermal image file
 thermal_image <- raster(paste0(pwd, "/Example data/Input data/", flight, map_name))
 
-# Step 2: Load the CSV file and extract the 'station-map' value
+#Load the CSV file and extract the correction value
 station_data <- fread(paste0(pwd, "/Example data/Tables/station_correction_values.csv"))
 correction_value <- station_data$`station - IR`[120]
 
-# Step 3: Apply correction (example: subtracting the station-map value)
+#Apply correction
 corrected_image <- thermal_image + correction_value
 
-# Step 4: Save the corrected image
+#Save the corrected image
 output_filename <- paste0(pwd, "/Example data/Input data/", flight, 'corrected_', map_name)
 writeRaster(corrected_image, output_filename, format = "GTiff", overwrite = TRUE)
 
